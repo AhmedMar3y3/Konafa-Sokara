14a539d840b19ce711ef6b01fdb5695710173b4c
ahmed taha <ahmedtahawork2001@gmail.com>
2025-01-10 16:12:26 +0200

    feat(Languages): add support for arabic languages

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -29,7 +29,7 @@ class RouteServiceProvider extends ServiceProvider
         });
 
         $this->routes(function () {
-            Route::middleware('api')
+            Route::middleware(['api','api_lang'])
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
 
