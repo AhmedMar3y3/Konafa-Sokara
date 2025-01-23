3098002244034a1c91f469993c8bf6f7a09ff2c4
Mar3y <ahmed.mariee.2002@gmail.com>
2025-01-23 11:31:07 +0200

    feat(Admin Dahsboard): Home Page design added

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,10 @@
 @extends('layout')
 @section('main')
-Let's Start Over!
-{{-- <div class="row">
+<div class="row" dir="rtl">
   <div class="col-sm-4 grid-margin">
     <div class="card">
       <div class="card-body">
-        <h3>Users</h3>
+        <h3>المستخدمين</h3>
         <div class="row">
           <div class="col-8 col-sm-12 col-xl-8 my-auto">
             <div class="d-flex d-sm-block d-md-flex align-items-center">
@@ -22,15 +21,15 @@ Let's Start Over!
   <div class="col-sm-4 grid-margin">
     <div class="card">
       <div class="card-body">
-        <h3>Places</h3>
+        <h3>المنتجات</h3>
         <div class="row">
           <div class="col-8 col-sm-12 col-xl-8 my-auto">
             <div class="d-flex d-sm-block d-md-flex align-items-center">
-              <h2 class="mb-0">{{ $places }}</h2>
+              <h2 class="mb-0">{{ $products }}</h2>
             </div>
           </div>
           <div class="col-4 col-sm-12 col-xl-4 text-center text-xl-right">
-            <i class="icon-lg fa fa-building text-danger ml-auto"></i>
+            <i class="icon-lg fa fa-cubes text-danger ml-auto"></i>
           </div>
         </div>
       </div>
@@ -39,27 +38,77 @@ Let's Start Over!
   <div class="col-sm-4 grid-margin">
     <div class="card">
       <div class="card-body">
-        <h3>Reviews</h3>
+        <h3>الفئات</h3>
         <div class="row">
           <div class="col-8 col-sm-12 col-xl-8 my-auto">
             <div class="d-flex d-sm-block d-md-flex align-items-center">
-              <h2 class="mb-0">{{ $reviews }}</h2>
+              <h2 class="mb-0">{{ $categories }}</h2>
             </div>
           </div>
           <div class="col-4 col-sm-12 col-xl-4 text-center text-xl-right">
-            <i class="icon-lg fa fa-star-half-empty text-success ml-auto"></i>
+            <i class="icon-lg fa fa-tags text-success ml-auto"></i>
           </div>
         </div>
       </div>
     </div>
   </div>
-</div>
+  <div class="col-sm-4 grid-margin">
+    <div class="card">
+      <div class="card-body">
+        <h3>الفئات الفرعية</h3>
+        <div class="row">
+          <div class="col-8 col-sm-12 col-xl-8 my-auto">
+            <div class="d-flex d-sm-block d-md-flex align-items-center">
+              <h2 class="mb-0">{{ $subcategories }}</h2>
+            </div>
+          </div>
+          <div class="col-4 col-sm-12 col-xl-4 text-center text-xl-right">
+            <i class="icon-lg fa fa-sitemap text-success ml-auto"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4 grid-margin">
+    <div class="card">
+      <div class="card-body">
+        <h3>الإضافات</h3>
+        <div class="row">
+          <div class="col-8 col-sm-12 col-xl-8 my-auto">
+            <div class="d-flex d-sm-block d-md-flex align-items-center">
+              <h2 class="mb-0">{{ $additions }}</h2>
+            </div>
+          </div>
+          <div class="col-4 col-sm-12 col-xl-4 text-center text-xl-right">
+            <i class="icon-lg fa fa-plus-circle text-info ml-auto"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4 grid-margin">
+    <div class="card">
+      <div class="card-body">
+        <h3>الطلبات</h3>
+        <div class="row">
+          <div class="col-8 col-sm-12 col-xl-8 my-auto">
+            <div class="d-flex d-sm-block d-md-flex align-items-center">
+              <h2 class="mb-0">{{ $orders }}</h2>
+            </div>
+          </div>
+          <div class="col-4 col-sm-12 col-xl-4 text-center text-xl-right">
+            <i class="icon-lg fa fa-shopping-cart text-warning ml-auto"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
 <!-- New Users Chart Section -->
 <div class="col-12">
-  <div class="card">
+  <div class="card rounded">
     <div class="card-body">
-      <h3 class="card-title">New Users <span>| Last 7 days</span></h3>
+      <h3 class="card-title"> المستخدمين الجدد <span>| اخر اسبوع</span></h3>
       <div id="newUsersChart"></div>
       <script>
      document.addEventListener("DOMContentLoaded", () => {
@@ -85,7 +134,7 @@ Let's Start Over!
         xaxis: { type: 'datetime', categories: last7Days },
         yaxis: {
             labels: {
-                formatter: function (val) { return Math.round(val) + " User"; }
+                formatter: function (val) { return Math.round(val) + " مستخدم"; }
             },
             min: 0,
             forceNiceScale: true
@@ -105,6 +154,6 @@ Let's Start Over!
       </script>
     </div>
   </div>
-</div> --}}
+</div>
 
 @endsection
